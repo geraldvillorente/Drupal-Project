631ae79f20268256132ec664dbe64e813cd92cb3
Jonathan Bardo <jonathan.bardo@x-team.com>
2014-01-09 13:52:41 -0800

    Add new files symlink

---
+++ sites/all/themes/xtheme/template.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Add body classes if certain regions have content.
+ */
+function xtheme_preprocess_html(&$variables) {
+  // if (!empty($variables['page']['featured'])) {
+  //   $variables['classes_array'][] = 'featured';
+  // }
+
+  // if (!empty($variables['page']['triptych_first'])
+  //   || !empty($variables['page']['triptych_middle'])
+  //   || !empty($variables['page']['triptych_last'])) {
+  //   $variables['classes_array'][] = 'triptych';
+  // }
+
+  // if (!empty($variables['page']['footer_firstcolumn'])
+  //   || !empty($variables['page']['footer_secondcolumn'])
+  //   || !empty($variables['page']['footer_thirdcolumn'])
+  //   || !empty($variables['page']['footer_fourthcolumn'])) {
+  //   $variables['classes_array'][] = 'footer-columns';
+  // }
+}
