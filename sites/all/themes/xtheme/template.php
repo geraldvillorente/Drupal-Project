72fc4a881d58ec185d608088d687945abf991f4d
Jonathan Bardo <jonathan.bardo@x-team.com>
2014-01-22 11:00:53 -0800

    Add new template file

--- sites/all/themes/xtheme/template.php
+++ sites/all/themes/xtheme/template.php
@@ -1,6 +1,8 @@
 <?php
 
-// Example process_page hook
+/**
+ * Example process_page hook.
+ */
 function xtheme_process_page(&$vars) {
-  $vars['logo'] = url( drupal_get_path('theme', 'xtheme').'/logo.png' );
+  $vars['logo'] = url(drupal_get_path('theme', 'xtheme') . '/logo.png');
 }
---
+++ sites/all/themes/xtheme/templates/xpoll-form.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Template file for the theming of the form page.
+ */
+?>
+
+<?php print drupal_render_children($form); ?>
